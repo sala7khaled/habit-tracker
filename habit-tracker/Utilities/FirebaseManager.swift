0602103bcf703c441f33bfe59fdd098054dac97d
sala7khaled <sala7khaled7@gmail.com>
2025-01-12 21:16:24 +0200

    add key struct to firebase manager

--- habit-tracker/Utilities/FirebaseManager.swift
+++ habit-tracker/Utilities/FirebaseManager.swift
@@ -8,11 +8,19 @@
 
 import FirebaseDatabase
 
+fileprivate struct FBKeys {
+    static let title = "title"
+    static let users = "users"
+    static let habits = "habits"
+    static let progress = "progress"
+}
+
 class FirebaseManager {
     
     // MARK: - Properties
     static let shared = FirebaseManager()
     private let databaseRef: DatabaseReference
+    private let usernameError = "Username is not set"
     
     // MARK: - Init
     private init() {
@@ -24,17 +32,17 @@ class FirebaseManager {
     /// Add habit
     func addHabit(habitTitle: String, completion: @escaping (String?) -> Void) {
         guard let username = Shared.username else {
-            completion("Username is not set")
+            completion(usernameError)
             return
         }
-        let habitId = databaseRef.child("users/\(username)/habits").childByAutoId().key
+        let habitId = databaseRef.child("\(FBKeys.users)/\(username)/\(FBKeys.habits)").childByAutoId().key
         let habitData: [String: Any] = ["title": habitTitle]
         
         guard let habitId = habitId else {
             completion("Failed to generate habitId")
             return
         }
-        databaseRef.child("users/\(username)/habits/\(habitId)").setValue(habitData, withCompletionBlock: { error, _ in
+        databaseRef.child("\(FBKeys.users)/\(username)/\(FBKeys.habits)/\(habitId)").setValue(habitData, withCompletionBlock: { error, _ in
             completion(error?.localizedDescription)
         })
     }
@@ -42,11 +50,11 @@ class FirebaseManager {
     /// Fetch habits
     func fetchHabits(completion: @escaping ([HabitModel]?, String?) -> Void) {
         guard let username = Shared.username else {
-            completion(nil, "Username is not set")
+            completion(nil, usernameError)
             return
         }
         
-        databaseRef.child("users/\(username)/habits").observe(.value) { snapshot in
+        databaseRef.child("\(FBKeys.users)/\(username)/\(FBKeys.habits)").observe(.value) { snapshot in
             if let habitsData = snapshot.value as? [String: [String: Any]] {
                 let habits = habitsData.compactMap { (id, data) -> HabitModel? in
                     return HabitModel(id: id, dictionary: data)
@@ -62,11 +70,11 @@ class FirebaseManager {
     /// Change habit's status for a specific date
     func changeHabitStatus(date: String, habitId: String, status: Bool, completion: @escaping (String?) -> Void) {
         guard let username = Shared.username else {
-            completion("Username is not set")
+            completion(usernameError)
             return
         }
         
-        let habitCompletionRef = databaseRef.child("users/\(username)/progress/\(date)/\(habitId)")
+        let habitCompletionRef = databaseRef.child("\(FBKeys.users)/\(username)/\(FBKeys.progress)/\(date)/\(habitId)")
         habitCompletionRef.setValue(status) { error, _ in
             completion(error?.localizedDescription)
         }
@@ -75,10 +83,10 @@ class FirebaseManager {
     /// Fetch all habits (completed + not completed) for a specific date
     func fetchAllHabitsStatus(forDate date: String, completion: @escaping ([HabitStatusModel]?, String?) -> Void) {
         guard let username = Shared.username else {
-            completion(nil, "Username is not set")
+            completion(nil, usernameError)
             return
         }
-        databaseRef.child("users/\(username)/progress/\(date)").observe(.value) { snapshot in
+        databaseRef.child("\(FBKeys.users)/\(username)/\(FBKeys.progress)/\(date)").observe(.value) { snapshot in
             if let completedHabits = snapshot.value as? [String: Bool] {
                 let habitStatuses = HabitStatusModel.fromDictionary(completedHabits)
                 completion(habitStatuses, nil)
@@ -91,10 +99,10 @@ class FirebaseManager {
     /// Delete a specific habit
     func deleteHabit(habitId: String, completion: @escaping (String?) -> Void) {
         guard let username = Shared.username else {
-            completion("Username is not set")
+            completion(usernameError)
             return
         }
-        databaseRef.child("users/\(username)/habits/\(habitId)").removeValue { error, _ in
+        databaseRef.child("\(FBKeys.users)/\(username)/\(FBKeys.habits)/\(habitId)").removeValue { error, _ in
             completion(error?.localizedDescription)
         }
     }
